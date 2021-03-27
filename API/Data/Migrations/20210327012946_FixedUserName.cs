0153f11362d602adb31181502e5bc3ccc765c593
ScottSmithGitHub <scott.smith1500@gmail.com>
2021-03-26 22:09:42 -0400

    Added Angular Project

---
+++ API/Data/Migrations/20210327012946_FixedUserName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class FixedUserName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UseName",
+                table: "Users",
+                newName: "UserName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "UseName");
+        }
+    }
+}
